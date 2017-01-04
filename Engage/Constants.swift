d8081b0bc8a75b9ecbb9b631c952d71cae7f3f9b
Nathan Tannar <nathantannar4@gmail.com>
2017-01-04 15:04:52 -0800

    Update Constants.swift

--- Engage/Constants.swift
+++ Engage/Constants.swift
@@ -16,10 +16,10 @@ import FontAwesomeKit
 /* Config */
 var VERSION = "1.1.0"
 var isWESST = false
-let APPLICATION_ID = "G0OYmfAMuI4ORbtWssrYWrwSfEqZbpxafRA8Mo2b"
-let CLIENT_KEY = "Ihk6kg7wyEHOvn914tYJw0ArgYzkzbrHp6TtZVNq"
-let SERVER_URL = "http://159.203.3.130:1337/parse"
-let FACEBOOK_APPLICATION_ID = "1328087523932712"
+let APPLICATION_ID = ""
+let CLIENT_KEY = ""
+let SERVER_URL = ""
+let FACEBOOK_APPLICATION_ID = ""
 
 var drawerController: DrawerController!
 
