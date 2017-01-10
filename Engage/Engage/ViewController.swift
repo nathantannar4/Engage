d2776cb780fb239b6703063ba771f7ed798efe3e
Nathan Tannar <nathan.tannar@sap.com>
2017-01-09 23:39:50 -0800

    Reset to Initial

---
+++ Engage/Engage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Engage
+//
+//  Created by Nathan Tannar on 1/9/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
